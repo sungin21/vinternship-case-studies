f6f153757f149faaa3ef1fb0c7023a6342e9c1e0
Sungini Rijhwani <rsungini@gmail.com>
2026-05-19 22:03:44 +0530

    Added let and const lesson

---
+++ typescript/lesson4-let-const/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let score = 90;
+console.log("Initial Score:", score);
+score = 95;
+console.log("Updated Score:", score);
+if (true) {
+    let score = 100;
+    console.log("Block Score:", score);
+}
+const COUNTRY = "India";
+console.log("Country:", COUNTRY);
+// Uncommenting below line will cause error
+// COUNTRY = "USA";
+if (true) {
+    const PI = 3.14;
+    console.log("PI Value:", PI);
+}
---
+++ typescript/lesson4-let-const/index.ts
@@ -0,0 +1,24 @@
+let score: number = 90;
+
+console.log("Initial Score:", score);
+
+score = 95;
+
+console.log("Updated Score:", score);
+
+if (true) {
+    let score: number = 100;
+    console.log("Block Score:", score);
+}
+
+const COUNTRY: string = "India";
+
+console.log("Country:", COUNTRY);
+
+// Uncommenting below line will cause error
+// COUNTRY = "USA";
+
+if (true) {
+    const PI: number = 3.14;
+    console.log("PI Value:", PI);
+}
